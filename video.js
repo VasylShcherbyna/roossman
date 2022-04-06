76fa24128479aa6526ca1d555ad31b0c4d761be5
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2022-04-06 08:49:34 +0200

    media 480

--- video.js
+++ video.js
@@ -1,31 +1,83 @@
-var myMp4 = document.getElementById("mp4"),
-	myWebm = document.getElementById("webm"),
-	myVid = document.getElementById("myVideo"),
-	mp4Url = "",
-	webmUrl = "";
+$(document).ready(function() {
+	$('#play-video').on('click', function(ev) {
+   
+	  $("#video")[0].src += "&autoplay=1";
+	  ev.preventDefault();
+   
+	});
+  });
 
-function playVid(ID) {
-	switch (ID) {
-		case "one":
-			mp4Url = "https://player.vimeo.com/external/207590826.hd.mp4?s=6a918d074abf8f3add7858018855524d384f6934&amp;profile_id=119";
-			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/blossoms.Webm";
-			break;
-		case "two":
-			mp4Url = "https://youtu.be/FHBziuKCHNg";
-			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/splash.Webm";
-			break;
-		case "three":
-			mp4Url = "https://www.uiuxwebdev.com/projects/videoGallery/transport.mp4";
-			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/transport.Webm";
-			break;
-		case "four":
-			mp4Url = "https://www.uiuxwebdev.com/projects/videoGallery/walking.mp4";
-			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/walking.Webm";
-			break;
-	}
-	myMp4.setAttribute("src", mp4Url);
-	myWebm.setAttribute("src", webmUrl);
-	myVid.load();
-	myVid.play();
-	myVid.poster = "";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myMp4 = document.getElementById("mp4"),
+// 	myWebm = document.getElementById("webm"),
+// 	myVid = document.getElementById("myVideo"),
+// 	mp4Url = "",
+// 	webmUrl = "";
+
+// function playVid(ID) {
+// 	switch (ID) {
+// 		case "one":
+// 			mp4Url = "https://player.vimeo.com/external/207590826.hd.mp4?s=6a918d074abf8f3add7858018855524d384f6934&amp;profile_id=119";
+// 			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/blossoms.Webm";
+// 			break;
+// 		case "two":
+// 			mp4Url = "https://youtu.be/FHBziuKCHNg";
+// 			webmUrl = "https://youtu.be/FHBziuKCHNg";
+// 			break;
+// 		case "three":
+// 			mp4Url = "https://www.uiuxwebdev.com/projects/videoGallery/transport.mp4";
+// 			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/transport.Webm";
+// 			break;
+// 		case "four":
+// 			mp4Url = "https://www.uiuxwebdev.com/projects/videoGallery/walking.mp4";
+// 			webmUrl = "https://www.uiuxwebdev.com/projects/videoGallery/walking.Webm";
+// 			break;
+// 	}
+// 	myMp4.setAttribute("src", mp4Url);
+// 	myWebm.setAttribute("src", webmUrl);
+// 	myVid.load();
+// 	myVid.play();
+// 	myVid.poster = "";
+// }
+
+
+
+// Silcare nappa 1 Substancje aktywne w codziennej pielęgnacji stóp Jak dbać o stopy #podolog - https://youtu.be/3KNkPf17qt8 
+// Silcare nappa 2 Jak dobrać stężenie mocznika w pielęgnacji Jak nawilżyć suche stopy #podolog - https://youtu.be/FHBziuKCHNg 
+// Silcare nappa 3 Pielęgnacja stóp krok po kroku Pielęgnacja stóp w trzech krokach #podolog - https://youtu.be/LpqmqU-NY5A 
+// Silcare nappa 4 Pedicure to nie tylko paznokcie! Jak poradzić sobie z problemami stóp #podolog - https://youtu.be/8iPKEe96BVw 
+// Silcare nappa 5 Peeling w pielęgnacji stóp Dlaczego i jak go używać #podolog - https://youtu.be/oma75nW8FFM 
+// Silcare nappa 6 Stopy są aktywne! Sport a zdrowie i higiena stóp #podolog - https://youtu.be/9xeCn-wFGVw 
+// Silcare nappa 7 Jak wygładzić i nawilżyć stopy Pielęgnacja suchej i szorstkiej skóry #podolog – https://youtu.be/6QxIAXBD-B8
+// Silcare nappa 8 Recepta na domową pielęgnację stóp Produkty na każdą potrzebę #podolog - https://youtu.be/lW2WvEID0nQ
+
+
